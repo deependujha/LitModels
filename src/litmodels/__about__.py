9f4bda17c978ab9b6bce215713452c5d2ac608f0
Jirka B <j.borovec+github@gmail.com>
2024-12-19 16:07:18 +0900

    Release 0.0.9

--- src/litmodels/__about__.py
+++ src/litmodels/__about__.py
@@ -1,8 +1,8 @@
-__version__ = "0.0.8rc"
+__version__ = "0.0.9rc"
 __author__ = "Lightning-AI et al."
 __author_email__ = "community@lightning.ai"
-__license__ = "Apache-2.0"  # TODO: change if needed and update the LICENSE file
-__copyright__ = f"Copyright (c) 2020-2022, {__author__}."
+__license__ = "Apache-2.0"
+__copyright__ = f"Copyright (c) 2024-2024, {__author__}."
 __homepage__ = "https://github.com/Lightning-AI/models"
 __docs__ = "Lightning AI Model hub."
 
